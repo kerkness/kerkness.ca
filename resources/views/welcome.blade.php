a198fba4ab4beed462e63db03e0a9da315cdb15a
kerkness <ryan@kerkness.ca>
2024-11-17 06:52:07 -0600

    ok

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,9 +11,8 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Styles / Scripts -->
-        @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
-            @vite(['resources/css/app.css', 'resources/js/app.js'])
-        @endif
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+
     </head>
     <body class="font-sans antialiased bg-pink-50 dark:bg-pink-900">
         <div class="text-black/50 dark:text-white/50">
